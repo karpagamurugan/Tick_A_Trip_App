5614555fac3c52462499ad248dba11df5767186a
karpagamurugan <109209421+karpagamurugan29@users.noreply.github.com>
2022-12-08 18:23:16 +0530

    navigation

---
+++ components/Home/Home.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ components/Home/Splash.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View,Text } from 'react-native';
+
+const Splash = () => {
+  return (
+    <View><Text>Splash</Text></View>
+  );
+};
+
+export default Splash;
